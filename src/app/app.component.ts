767e41531be6f13e6b657b9859594ba4334a6b30
KennyTheWizard <kenngineering@gmail.com>
2017-11-08 23:02:05 -0600

    intial commit with tz working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  theDate: Date;
+  tzSelect = { 
+    pst : false,
+    mst : false,
+    cst : false,
+    est : false,
+  };
+  currZone: string = '';
+  changeTimeZone(zone:string):void{
+    for (let tZone in this.tzSelect){
+      this.tzSelect[tZone] = (tZone == zone);
+    }
+    this.currZone = zone;
+    console.log(zone);
+    this.theDate = new Date();
+  }
+  
 }
